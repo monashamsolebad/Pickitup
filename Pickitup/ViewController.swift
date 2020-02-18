527f3eba10df2cde218ca95cb775a593b6d32414
monashamsolebad <shamsolebad.mona@gmail.com>
2020-02-17 23:23:37 -0800

    Initial Commit

---
+++ Pickitup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pickitup
+//
+//  Created by Mona Shamsolebad on 2020-02-17.
+//  Copyright © 2020 Pickitup Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
